0e52766d2055b483ad5f4dfc314a86d9570326c9
Joshua Butler <35884919+ButlerJAL@users.noreply.github.com>
2025-11-24 12:41:07 -0500

    Implement chat and messaging functionality with model integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :chats do
+    resources :messages, only: [:create]
+  end
+  resources :models, only: [:index, :show] do
+    collection do
+      post :refresh
+    end
+  end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
