b172ee72e19e9dce6d2b4c2b6a2f37c6c0cdddc2
Joshua Butler <35884919+ButlerJAL@users.noreply.github.com>
2025-11-26 17:31:04 -0500

    feat: update views for improved layout and styling; modify authentication flow in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
-
   def home
+    if user_signed_in?
+      redirect_to challenges_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
