4726b669bef5d0b60e98911e7716cd7725e59bba
Joshua Butler <35884919+ButlerJAL@users.noreply.github.com>
2025-12-03 14:49:58 -0500

    Refactor routes to remove unused resources and organize trauma-related routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
-  resources :models, only: %i[index show] do
-    collection do
-      post :refresh
-    end
-  end
-  resources :chats do
-    resources :messages, only: [:create]
-  end
-  resources :models, only: %i[index show] do
-    collection do
-      post :refresh
-    end
-  end
-  devise_for :users
-  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  # Defines devise routes for user authentication
+  devise_for :users
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: 'traumas#index'
+  # Defines routes for traumas resource
+  resources :traumas, only: %i[create edit update destroy] do
+    resources :stuck_points, only: %i[index create edit update destroy] do
+      resources :abc_worksheets, only: %i[index create edit update destroy]
+      resources :alternative_thoughts, only: %i[index create edit update destroy]
+    end
+  end
 end
